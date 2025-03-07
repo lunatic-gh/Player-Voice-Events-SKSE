b0da23ee01c6d54c349d0aa5bf5c44bc5cb66420
Luna <luna@chloedev.de>
2025-03-07 23:43:16 +0100

    Upload Files

--- plugin.cpp
+++ plugin.cpp
@@ -1,24 +1,14 @@
-#include "EventHandler.h"
+void StopSound(RE::StaticFunctionTag*) {
+    RE::PlayerCharacter::GetSingleton()->PauseCurrentDialogue();
+}
 
-void OnPlayerActivateItem(std::string itemName) {
-    // This is the message that we want to show
-    auto message = std::format("Player activated {}", itemName);
-
-    // Popup a "Debug MessageBox" (with an OK button)
-    RE::DebugMessageBox(message.c_str());
-
-    // Show a "Debug Notification" (displays in the top-left corner of the game)
-    RE::DebugNotification(message.c_str());
+bool PapyrusFuncs(RE::BSScript::IVirtualMachine* vm) {
+    vm->RegisterFunction("StopSound", "LNTC_PVESKSEFunctions", StopSound);
+    return true;
 }
 
 SKSEPluginLoad(const SKSE::LoadInterface* skse) {
     SKSE::Init(skse);
-    // We need to do *something* in the game to activate our "MessageBox" and "Notification".
-    // This registers an "EventHandler" to handle whenever any object in the game is "Activated".
-    // If the *player* activated the object, the name of the activated object is passed to OnPlayerActivateItem() above.
-    // If you are curious about the event handler, see EventHandler.h
-    // But you can also see more examples of game event handlers in the template:
-    //                                                       https://github.com/SkyrimScripting/SKSE_Template_GameEvents
-    RE::ScriptEventSourceHolder::GetSingleton()->AddEventSink<RE::TESActivateEvent>(new EventHandler());
+    SKSE::GetPapyrusInterface()->Register(PapyrusFuncs);
     return true;
 }
